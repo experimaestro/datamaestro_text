7fa6b5e0b881d91bc033f578dd95da2f8e6c7354
Benjamin Piwowarski <benjamin@piwowarski.fr>
2019-11-04 10:19:10 +0100

    Option for full parsing

--- datamaestro_text/handlers/files/tagging.py
+++ datamaestro_text/handlers/files/tagging.py
@@ -2,13 +2,17 @@ import logging
 from datamaestro.handlers.files import File
 
 class CoNLL_U(File):
-    def data(self):
+    def data(self, generator=False):
         try:
-            from conllu import parse_incr
+            from conllu import parse_incr, parse
         except:
             logging.error("conllu python module not installed")
             raise 
 
         data_file = self.path.open("r", encoding="utf-8")
+
+        if not generator:
+            return parse(data_file)
+            
         for tokenlist in parse_incr(data_file):
             yield tokenlist
