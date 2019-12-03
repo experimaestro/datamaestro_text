76683429b77743149d5b9213337de17aaabc941f
Benjamin Piwowarski <benjamin@piwowarski.fr>
2019-12-03 09:51:34 +0100

    Fix Stanford + encodings

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ This [datasets](https://github.com/bpiwowar/datasets) plugin covers text-related
 - Natural Language Processing tasks
 
 The list of available datasets and usage instruction can be found in the [documentation](http://bpiwowar.github.io/datamaestro_text/).
+
+## List of available datasets
+
+### Word embeddings
+
+- [Glove](http://nlp.stanford.edu/projects/glove/) `edu.stanford.glove.XXXX`
+
+### Sentiment analysis
+
+- [IMDB]() `edu.stanford.aclimdb`
--- datamaestro_text/data/embeddings.py
+++ datamaestro_text/data/embeddings.py
@@ -1,14 +1,15 @@
 from pathlib import Path
-from datamaestro.data import File, Data
+from datamaestro.data import File, Data, Argument
 import numpy as np
 
+@Argument("encoding", str, ignored=True, default="utf-8")
 @Data(description="Word embeddings as a text word / values")
 class WordEmbeddingsText(File):
     def load(self):
         words = {}
         vectors = []
         dimension = None
-        with self.path.open("rt") as fp:
+        with self.path.open("rt", encoding=self.encoding) as fp:
             for ix, line in enumerate(fp):
                 word, *values = line.split(" ")
                 vectors.append([float(x) for x in values])
