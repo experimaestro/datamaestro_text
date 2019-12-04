6aa7f72c2dc090842720a28fdc2c3d1ad13e9789
Benjamin Piwowarski <benjamin@piwowarski.fr>
2019-12-04 16:18:41 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import re
 import sys
-
 try:
     from setuptools import setup, find_namespace_packages
 except ImportError:
@@ -15,6 +14,22 @@ RE_BLANCK=re.compile(r"^\s*(#.*)?$")
 with (Path(__file__).parent / 'requirements.txt').open() as f:
     requirements = [x for x in f.read().splitlines() if not RE_BLANCK.match(x)]
 
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='datamaestro_text',
     version=VERSION,
@@ -25,6 +40,8 @@ setup(
     license='MIT',
     python_requires='>=3.5',
     packages=find_namespace_packages(include="datamaestro_text.*"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 
     install_requires=requirements,
     classifiers=[
