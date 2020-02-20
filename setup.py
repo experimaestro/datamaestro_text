d6ddb2f3c1be2439d14692e7a4a8373b55956e92
Benjamin Piwowarski <benjamin@piwowarski.fr>
2020-02-20 18:03:33 +0100

    Use SCM version

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from pathlib import Path
 from setuptools import setup
 
-setup(install_requires=Path("requirements.txt").read_text())
+basepath = Path(__file__).parent
+
+setup(
+    install_requires=(basepath / "requirements.txt").read_text(), use_scm_version=True,
+)
