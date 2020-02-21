06d15a67b30d960159ecb1e02e73b283739f0655
Benjamin Piwowarski <benjamin@piwowarski.fr>
2020-02-21 15:21:57 +0100

    Switch to datafolderpath

--- datamaestro_text/config/gov/nist/trec/tipster.py
+++ datamaestro_text/config/gov/nist/trec/tipster.py
@@ -20,33 +20,40 @@ See also https://trec.nist.gov/data/docs_eng.html and https://trec.nist.gov/data
 from datamaestro.data import Base
 from datamaestro_text.data.trec import TipsterCollection
 from datamaestro.download.manual import linkfolder
-from datamaestro.definitions import data, argument, datatasks, datatags, dataset
+from datamaestro.definitions import (
+    data,
+    argument,
+    datatasks,
+    datatags,
+    dataset,
+    DatafolderPath,
+)
 
 TIPSTER = dataset(TipsterCollection, url="https://catalog.ldc.upenn.edu/LDC93T3A")
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk2/AP"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk2/AP")])
 @TIPSTER
 def ap88(documents):
     """Associated Press document collection (1988)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk1/AP"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk1/AP")])
 @TIPSTER
 def ap89(documents):
     """Associated Press document collection (1989)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk3/AP"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk3/AP")])
 @TIPSTER
 def ap90(documents):
     """Associated Press document collection (1990)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk1/DOE"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk1/DOE")])
 @TIPSTER
 def doe1(documents):
     """Department of Energy documents"""
@@ -56,42 +63,42 @@ def doe1(documents):
 # --- Wall Street Journal (1987-92)
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk1/WSJ/1987"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk1/WSJ/1987")])
 @TIPSTER
 def wsj87(documents):
     """Wall Street Journal (1987)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk1/WSJ/1988"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk1/WSJ/1988")])
 @TIPSTER
 def wsj88(documents):
     """Wall Street Journal (1988)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk1/WSJ/1989"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk1/WSJ/1989")])
 @TIPSTER
 def wsj89(documents):
     """Wall Street Journal (1989)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk2/WSJ/1990"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk2/WSJ/1990")])
 @TIPSTER
 def wsj90(documents):
     """Wall Street Journal (1990)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk2/WSJ/1991"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk2/WSJ/1991")])
 @TIPSTER
 def wsj91(documents):
     """Wall Street Journal (1991)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk2/WSJ/1992"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk2/WSJ/1992")])
 @TIPSTER
 def wsj92(documents):
     """Wall Street Journal (1992)"""
@@ -101,21 +108,21 @@ def wsj92(documents):
 # --- Federal Register (1988-89)
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk2/FR"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk2/FR")])
 @TIPSTER
 def fr88(documents):
     """Federal Register (1988)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk1/FR"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk1/FR")])
 @TIPSTER
 def fr89(documents):
     """Federal Register (1989)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk4/FR94"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk4/FR94")])
 @TIPSTER
 def fr94(documents):
     """Federal Register (1994)"""
@@ -125,56 +132,56 @@ def fr94(documents):
 # # ZIFF (1988-92)
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk1/ZIFF"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk1/ZIFF")])
 @TIPSTER
 def ziff1(documents):
     """Information from the Computer Select disks (1989-90)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk2/ZIFF"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk2/ZIFF")])
 @TIPSTER
 def ziff2(documents):
     """Information from the Computer Select disks (1989-90)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk3/ZIFF"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk3/ZIFF")])
 @TIPSTER
 def ziff3(documents):
     """Information from the Computer Select disks (1990-91)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk3/SJM"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk3/SJM")])
 @TIPSTER
 def sjm1(documents):
     """San Jose Mercury News (1991)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk4/CR"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk4/CR")])
 @TIPSTER
 def cr1(documents):
     """TODO"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk4/FT"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk4/FT")])
 @TIPSTER
 def ft1(documents):
     """Financial Times"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk5/FBIS"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk5/FBIS")])
 @TIPSTER
 def fbis1(documents):
     """Foreign Broadcast Information Service (1996)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{gov.nist.trec.tipster}}/Disk5/LATIMES"])
+@linkfolder("documents", [DatafolderPath("gov.nist.trec.tipster", "Disk5/LATIMES")])
 @TIPSTER
 def la8990(documents):
     """Los Angeles Times (1989-90)"""
