58a3e8c818d5db74cda2d31865a80302d910d9d8
Benjamin Piwowarski <benjamin@piwowarski.fr>
2020-02-25 13:23:23 +0100

    Correct AQUAINT collection

--- datamaestro_text/config/edu/upenn/ldc/aquaint.py
+++ datamaestro_text/config/edu/upenn/ldc/aquaint.py
@@ -1,5 +1,6 @@
 """The AQUAINT Corpus, Linguistic Data Consortium (LDC) catalog number LDC2002T31 and ISBN 1-58563-240-6 consists of newswire text data in English, drawn from three sources: the Xinhua News Service (People's Republic of China), the New York Times News Service, and the Associated Press Worldstream News Service. It was prepared by the LDC for the AQUAINT Project, and will be used in official benchmark evaluations conducted by National Institute of Standards and Technology (NIST)."""
 
+from datamaestro.definitions import DatafolderPath
 from datamaestro.data import Base
 from datamaestro_text.data.trec import TipsterCollection
 from datamaestro.download.manual import linkfolder
@@ -10,21 +11,21 @@ from datamaestro.download.links import Links
 URL = "https://catalog.ldc.upenn.edu/LDC2002T31"
 
 
-@linkfolder("documents", ["{{edu.upenn.ldc.aquaint}}/APW"])
+@linkfolder("documents", [DatafolderPath("edu.upenn.ldc.aquaint", "APW")])
 @dataset(TipsterCollection, url=URL, id="apw")
 def apw(documents):
     """Associated Press (1998-2000)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{edu.upenn.ldc.aquaint}}/NYT"])
+@linkfolder("documents", [DatafolderPath("edu.upenn.ldc.aquaint", "NYT")])
 @dataset(TipsterCollection, url=URL, id="nyt")
 def nyt(documents):
     """New York Times (1998-2000)"""
     return {"path": documents}
 
 
-@linkfolder("documents", ["{{edu.upenn.ldc.aquaint}}/XIE"])
+@linkfolder("documents", [DatafolderPath("edu.upenn.ldc.aquaint", "XIE")])
 @dataset(TipsterCollection, url=URL, id="xie")
 def xie(documents):
     """Xinhua News Agency newswires (1996-2000)"""
