31cf4cf6e42d9f34ccff2c08ea84320736077fab
Benjamin Piwowarski <benjamin@piwowarski.fr>
2020-12-20 14:57:13 +0100

    Methods to access queries and qrels (TREC)

--- datamaestro_text/data/ir/trec.py
+++ datamaestro_text/data/ir/trec.py
@@ -3,20 +3,33 @@ from datamaestro.definitions import argument, data
 from experimaestro import Any
 from datamaestro.data import Base
 from pathlib import Path
-from datamaestro_text.data.ir import AdhocDocuments, AdhocTopics, AdhocAssessments, AdhocRun
+from datamaestro_text.data.ir import (
+    AdhocDocuments,
+    AdhocTopics,
+    AdhocAssessments,
+    AdhocRun,
+)
 
 
 @argument("path", type=Path)
 @argument("parts", type=Any)
 @data()
 class TrecAdhocTopics(AdhocTopics):
-    pass
+    def iter(self):
+        """Iterate over TREC adhoc topics"""
+        import datamaestro_text.interfaces.trec as trec
+
+        yield from trec.parse_query_format(self.path)
 
 
 @argument("path", type=Path)
 @data()
 class TrecAdhocAssessments(AdhocAssessments):
-    pass
+    def iter(self):
+        """Iterate over TREC adhoc topics"""
+        import datamaestro_text.interfaces.trec as trec
+
+        yield from trec.parse_qrels(self.path)
 
 
 @argument("path", type=Path)
@@ -24,11 +37,17 @@ class TrecAdhocAssessments(AdhocAssessments):
 class TrecAdhocRun(AdhocRun):
     pass
 
-@argument("metrics", required=False, type=List[str], help="List of reported metrics (None if all from trec_eval)")
+
+@argument(
+    "metrics",
+    required=False,
+    type=List[str],
+    help="List of reported metrics (None if all from trec_eval)",
+)
 @argument("results", type=Path, help="Main results")
 @argument("detailed", type=Path, required=False, help="Results per topic (if any)")
 @data()
-class TrecAdhocResults():
+class TrecAdhocResults:
     pass
 
 
---
+++ datamaestro_text/interfaces/trec.py
@@ -0,0 +1,83 @@
+from pathlib import Path
+from typing import List, NamedTuple, Optional
+
+
+class Assessment(NamedTuple):
+    docno: str
+    rel: float
+
+
+class AssessedTopic(NamedTuple):
+    qid: str
+    assessments: List[Assessment]
+
+
+def parse_qrels(path: Path):
+    with path.open("rt") as fp:
+        _qid = None
+        assessments = None
+
+        for line in fp:
+            qid, _, docno, rel = line.strip().split(" ")
+            if qid != _qid:
+                if _qid is not None:
+                    yield AssessedTopic(_qid, assessments)
+                _qid = qid
+                assessments = []
+            assessments.append(Assessment(docno, rel))
+
+        yield AssessedTopic(_qid, assessments)
+
+
+# ---- TOPICS
+
+
+class Topic(NamedTuple):
+    num: str
+    title: str
+    desc: str
+    narr: str
+
+
+def cleanup(s: Optional[str]):
+    return s.replace("\t", " ").strip() if s is not None else None
+
+
+def parse_query_format(file, xml_prefix=None):
+    """Parse TREC XML query format"""
+    if xml_prefix is None:
+        xml_prefix = ""
+
+    if hasattr(file, "read"):
+        num, title, desc, narr, reading = None, None, None, None, None
+        for line in file:
+            if line.startswith("**"):
+                # translation comment in older formats (e.g., TREC 3 Spanish track)
+                continue
+            elif line.startswith("</top>"):
+                yield Topic(num, cleanup(title), cleanup(desc), cleanup(narr))
+                num, title, desc, narr, reading = None, None, None, None, None
+            elif line.startswith("<num>"):
+                num = line[len("<num>") :].replace("Number:", "").strip()
+                reading = None
+            elif line.startswith(f"<{xml_prefix}title>"):
+                title = line[len(f"<{xml_prefix}title>") :].strip()
+                if title == "":
+                    reading = "title"
+                else:
+                    reading = None
+            elif line.startswith(f"<{xml_prefix}desc>"):
+                desc = ""
+                reading = "desc"
+            elif line.startswith(f"<{xml_prefix}narr>"):
+                narr = ""
+                reading = "narr"
+            elif reading == "desc":
+                desc += line.strip() + " "
+            elif reading == "narr":
+                narr += line.strip() + " "
+            elif reading == "title":
+                title += line.strip() + " "
+    else:
+        with open(file, "rt") as f:
+            yield from parse_query_format(f)
