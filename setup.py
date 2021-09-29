9a02864fe7b5a01d622377a26d9fc1f3c62fdf5c
Benjamin Piwowarski <benjamin@piwowarski.fr>
2021-09-29 11:46:10 +0200

    Fix test with github workflows

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from pathlib import Path
 from setuptools import setup
+import os
 
 basepath = Path(__file__).parent
 
+version = None
+if os.environ.get("NO_GIT", 0) == "1":
+    version = "0.0.0-dev"
+
 setup(
-    install_requires=(basepath / "requirements.txt").read_text(), use_scm_version=True,
+    install_requires=(basepath / "requirements.txt").read_text(),
+    use_scm_version=version is None,
+    version=version,
 )
