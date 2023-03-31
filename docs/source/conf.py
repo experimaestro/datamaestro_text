9202a846fe60b82a55fb17e247acef422dcae6ec
Benjamin Piwowarski <benjamin@piwowarski.fr>
2023-03-31 17:24:44 +0200

    docs: Use datamaestro version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-from xpmir import version
+from datamaestro_text import version
 
 # -- Project information -----------------------------------------------------
 
